727c822f918fd78375f2f5aa77e5da524d5fe69f
yaumil <yaumilramkhadani@gmail.com>
2025-12-20 17:06:05 +0700

    adding colors

--- lib/utils/values/colors/colors.dart
+++ lib/utils/values/colors/colors.dart
@@ -1,9 +1,10 @@
 import 'dart:ui';
 
-class Colors {
+class MyColors {
   static const int _alphaCode = 0xff;
-  static const brown500 = Color(_alphaCode << 0x4E342E);
-  static const brown400 = Color(_alphaCode << 0x8D6E63);
-  static const brown300 = Color(_alphaCode << 0x8A1887F);
-  static const brown200 = Color(_alphaCode << 0xBCAAA4);
+  static const int _slideToLeft = 24; // geser ke kiri 24 bit
+  static const brown500 = Color(_alphaCode << _slideToLeft | 0x4E342E);
+  static const brown400 = Color(_alphaCode << _slideToLeft | 0x8D6E63);
+  static const brown300 = Color(_alphaCode << _slideToLeft | 0x8A1887F);
+  static const brown200 = Color(_alphaCode << _slideToLeft | 0xBCAAA4);
 }
