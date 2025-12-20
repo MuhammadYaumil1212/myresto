9c345a865d8b59a317552ffd70e837629ceb715e
yaumil <yaumilramkhadani@gmail.com>
2025-12-20 16:57:31 +0700

    adding colors

--- lib/utils/values/colors/colors.dart
+++ lib/utils/values/colors/colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 class Colors {
-  static const _alphaCode = 0xff;
+  static const int _alphaCode = 0xff;
   static const brown500 = Color(_alphaCode << 0x4E342E);
   static const brown400 = Color(_alphaCode << 0x8D6E63);
   static const brown300 = Color(_alphaCode << 0x8A1887F);
