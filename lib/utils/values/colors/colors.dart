6242d0cb937ee7f8479cbc6fd863b51395e3877d
yaumil <yaumilramkhadani@gmail.com>
2025-12-21 19:54:14 +0700

    change colors hexa

--- lib/utils/values/colors/colors.dart
+++ lib/utils/values/colors/colors.dart
@@ -1,10 +1,8 @@
 import 'dart:ui';
 
 class MyColors {
-  static const int _alphaCode = 0xff;
-  static const int _slideToLeft = 24; // geser ke kiri 24 bit
-  static const brown500 = Color(_alphaCode << _slideToLeft | 0x4E342E);
-  static const brown400 = Color(_alphaCode << _slideToLeft | 0x8D6E63);
-  static const brown300 = Color(_alphaCode << _slideToLeft | 0x8A1887F);
-  static const brown200 = Color(_alphaCode << _slideToLeft | 0xBCAAA4);
+  static const brown500 = Color(0xff4E342E);
+  static const brown400 = Color(0xff8D6E63);
+  static const brown300 = Color(0xffA1887F);
+  static const brown200 = Color(0xffBCAAA4);
 }
