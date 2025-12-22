e904d3343bf147c9ce2344f55bd2f80b4ee2c10b
yaumil <yaumilramkhadani@gmail.com>
2025-12-22 17:32:03 +0700

    setting debugShowBannerMode

--- lib/utils/values/themes/themes.dart
+++ lib/utils/values/themes/themes.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:myresto/utils/values/colors/colors.dart';
 
 class Themes {
   static final myTheme = ThemeData(
-    colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+    scaffoldBackgroundColor: Colors.white,
+    brightness: .light,
+    useMaterial3: true,
+    colorScheme: .fromSeed(seedColor: MyColors.brown200),
   );
 }
